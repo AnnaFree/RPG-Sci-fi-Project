84c85637658f448f4aacb5aec08ff015e468b54d
AnnaFree <annagrace660@gmail.com>
2024-12-17 01:57:14 -0500

    Running blend animation

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,11 +6,12 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
     {
-        
+        // Debug.Log(GetComponent<NavMeshAgent>().velocity);
     }
 
     // Update is called once per frame
@@ -19,9 +20,13 @@ public class Mover : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             MoveToCursor();
-            
+
+           
+
         }
+        UpdateAnimator();
     }
+    
     private void MoveToCursor()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -30,6 +35,20 @@ public class Mover : MonoBehaviour
         if (hasHit)
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
+           
+            Debug.Log("Destination: " + hit.point);
+           
         }
+    }
+    private void UpdateAnimator()
+    {//transform to a local to give the information more easily to the animator
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        //all we need to know is the z speed to know how fast we are going
+        float speed = localVelocity.z;
+        
+        //string references are not ideal but sometimes you have to use them
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+       
     }
 }
