dc728f5c89cde24704d1b0a43c8c5f4b943d97fb
AnnaFree <annagrace660@gmail.com>
2024-12-28 00:11:57 -0500

    Combat with Target Part 1
    Selecting target of an attack using Raycast hit class as well as other defined namespaces

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public void Attack(CombatTarget target)
+        {
+
+        }
+    }
+
+}
+
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,23 +1,45 @@
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
+using System;
+
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
         void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target != null) continue;
+
+                if (Input.GetMouseButtonDown(0)) {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
-
             }
-
         }
+
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
@@ -25,6 +47,11 @@ namespace RPG.Control
                 Debug.Log("Destination: " + hit.point);
 
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 
