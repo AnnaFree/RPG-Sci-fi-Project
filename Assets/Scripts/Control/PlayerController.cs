595dcb2dfe33512433edbb142b7ce48148fc650d
AnnaFree <annagrace660@gmail.com>
2024-12-28 00:22:55 -0500

    Action Priority beginning implement and method consolidation
    InteractWithMovement has been combined with MoveToCursor. We also now have the setup for a action affordance hint for plays (logic created but not fully implemented)

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -9,12 +9,12 @@ namespace RPG.Control
     {
         void Update()
         {
-            InteractWithCombat();
-            InteractWithMovement();
+            if (InteractWithCombat()) return;
+            if(InteractWithMovement()) return;
 
         }
 
-        private void InteractWithCombat()
+        private bool InteractWithCombat()
         {
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
             foreach (RaycastHit hit in hits)
@@ -25,28 +25,26 @@ namespace RPG.Control
                 if (Input.GetMouseButtonDown(0)) {
                     GetComponent<Fighter>().Attack(target);
                 }
+                return true;
             }
+            return false;
         }
 
-        private void InteractWithMovement()
+        private bool InteractWithMovement()
         {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
-        }
-
-        private void MoveToCursor()
-        {
+           
             RaycastHit hit;
             bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
-                GetComponent<Mover>().MoveTo(hit.point);
-
-                Debug.Log("Destination: " + hit.point);
+                if (Input.GetMouseButton(0))
+                {
+                    GetComponent<Mover>().MoveTo(hit.point);
+                }
+                return true;
 
             }
+            return false;
         }
 
         private static Ray GetMouseRay()
